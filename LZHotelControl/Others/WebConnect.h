c81ffb303dcf31fffa2cf1f3a7c3ac41dd971edb
Jony <jony@AC900166.ipt.aol.com>
2017-06-15 17:27:45 +0800

    add HTTP get request

---
+++ LZHotelControl/Others/WebConnect.h
@@ -0,0 +1,13 @@
+//
+//  WebConnect.h
+//  LZHotelControl
+//
+//  Created by Jony on 17/6/15.
+//  Copyright © 2017年 yavatop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebConnect : NSObject
+
+@end
---
+++ LZHotelControl/Others/WebConnect.m
@@ -0,0 +1,13 @@
+//
+//  WebConnect.m
+//  LZHotelControl
+//
+//  Created by Jony on 17/6/15.
+//  Copyright © 2017年 yavatop. All rights reserved.
+//
+
+#import "WebConnect.h"
+
+@implementation WebConnect
+
+@end
